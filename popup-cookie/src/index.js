3fa616df3d70fb803dd7a7ca7b0d23c544bb334d
jihye <byjihye@hashsnap.net>
2020-10-28 18:24:29 +0900

    팝업쿠키설정 - 만료시간 설정부분 일반화

--- popup-cookie/src/index.js
+++ popup-cookie/src/index.js
@@ -1,35 +1,44 @@
 /**
+ * cookie 옵션 
  * secure: https 에서만 쿠키 사용
  * expires/max-age: 쿠키의 만료 시간 설정
  * expires는 date 객체
  * max-age는 초 단위(3600 1시간)
  */
 
-export const setCookie = ({name, value, days}) => {
-  const date = new Date();
-  date.setTime(date.getTime() + (days*24*60*60*1000));
-  const expires = date.toUTCString();
+export const timeParser = (strs, ...props) => strs.reduce((acc, val) => acc += (props.shift() ?? '') + val).split(' ').map(str => {
+  const value = Number(str.slice(0, -1));
+  switch (str.slice(-1)) {
+  case 'D':
+  case 'd': return value * 24 * 60 * 60 * 1000;
+  case 'h': return value * 60 * 60 * 1000;
+  case 'm': return value * 60 * 1000;
+  case 's': return value * 1000;
+  }
+}).reduce((acc, val) => acc += val);
 
+export const setCookie = (name, value, time) => {
+  const now = new Date();
+  now.setTime(now.getTime() + time);
+  const expires = now.toUTCString();
   const cookie = encodeURIComponent(name)+'='+encodeURIComponent(value)+';';
-  const updatedCookie = days ? cookie+'expires='+expires+';path=/;' : cookie+'path=/;'
+  const updatedCookie = time ? cookie+'expires='+expires+';path=/;' : cookie+'path=/;'
   document.cookie = updatedCookie;
   console.log('cookie:', updatedCookie);
+
   return {name, value}
 }
-
-const cookie = setCookie({
-  name: 'popupClose',
-  value: 'today',
-  days: 1
-});
-
-export const checkCookie = popupId => {
+export const checkCookie = (cookie, element) => {
   const cookieData = Object.values(cookie).join('=');
   if(document.cookie.indexOf(cookieData) < 0){
-    document.getElementById(popupId).style.display = "block";
+    element.style.display = "block";
   }else{
-    document.getElementById(popupId).style.display = "none";
+    element.style.display = "none";
   }
 }
+const cookie = setCookie('popupClose', 'today', timeParser`1d 2h`);
+checkCookie(cookie, document.getElementById('app'))
+
+
 
 
